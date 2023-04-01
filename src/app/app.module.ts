6f7161204f75bf662ba8c2203e09f50f8e6c7a08
Maksym Kultyshev <mksbnt@icloud.com>
2023-04-01 18:07:18 +0300

    upd modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,33 @@ import {BrowserModule} from '@angular/platform-browser';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {LeafletDrawModule} from '@asymmetrik/ngx-leaflet-draw';
 import {AppComponent} from './app.component';
-import {RouterOutlet} from "@angular/router";
+import {RouterModule, RouterOutlet} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
-import { PointDialogComponent } from './pages/map/point-dialog/point-dialog.component';
+import {PointDialogComponent} from './pages/map/point-dialog/point-dialog.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
-import {MatDialogModule} from "@angular/material/dialog";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
 import {UpdatePointNumbersPipe} from './core/pipes/update-point-numbers.pipe';
-import { IncrementPipe } from './core/pipes/increment.pipe';
-import { LocationsPipe } from './core/pipes/locations.pipe';
+import {IncrementPipe} from './core/pipes/increment.pipe';
+import {LocationsPipe} from './core/pipes/locations.pipe';
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
-  declarations: [AppComponent, PointDialogComponent, UpdatePointNumbersPipe, IncrementPipe, LocationsPipe],
+  declarations: [
+    AppComponent,
+    PointDialogComponent,
+    UpdatePointNumbersPipe,
+    IncrementPipe,
+    LocationsPipe
+  ],
   imports: [
     BrowserModule,
     LeafletModule,
     LeafletDrawModule,
+    RouterModule,
     RouterOutlet,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -30,9 +38,19 @@ import { LocationsPipe } from './core/pipes/locations.pipe';
     MatInputModule,
     MatDialogModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [UpdatePointNumbersPipe, IncrementPipe, LocationsPipe],
+  providers: [
+    UpdatePointNumbersPipe,
+    IncrementPipe,
+    LocationsPipe,
+    MatDialog,
+  ],
+  exports: [
+    RouterModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
